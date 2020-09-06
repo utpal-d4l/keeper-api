c3f1680cc5fd2358bbd065f09056ea551f0dbb79
Utpal Singh <utpal@beecash.io>
2020-09-06 23:11:17 +0530

    added cors origins

--- app.js
+++ app.js
@@ -9,9 +9,23 @@ const { noteRoutes, userRoutes } = require('./src/routes/index')
 
 const app = express()
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://keeper-2c245.web.app',
+  'https://keeper-2c245.firebaseapp.com'
+]
+
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
